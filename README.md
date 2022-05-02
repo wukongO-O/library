02ca29e4dd7309583b84b417d952cac54fc7d854
wukongO-O <l.shenglan@gmail.com>
2022-05-01 23:29:38 -0400

    add styles & organize js and html code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# library
+This project uses objects and object constructors to build a library. 
+
+Souce for background image: The Washington herald. [volume] (Washington, D.C.), 01 May 1922. Chronicling America: Historic American Newspapers. Lib. of Congress. <https://chroniclingamerica.loc.gov/lccn/sn83045433/1922-05-01/ed-1/seq-1/>
--- library.js
+++ library.js
@@ -18,35 +18,44 @@ the1619.addBookToLibrary();
 animalFarm.addBookToLibrary();
 mahjong.addBookToLibrary();
 
-//create a card for each book object by DOM methods - its content includes book info, read-yet toggle status, and a delete button
+//use DOM methods to create a card displaying a book object - its content includes book info, read-yet toggle status, and a delete button
 const cards = document.querySelector('.display');
 Book.prototype.display = function() {
     const card = document.createElement('div');
     card.classList.add('libBook');
     cards.appendChild(card);
-    for (let prop in this) {
-        const info=document.createElement('p');
-        info.classList.add('itemInfo');
-        card.appendChild(info);
+    const div0 = document.createElement('div');
+    div0.classList.add('content');
+    card.appendChild(div0);
+    for (let prop in this) { 
         let ownProp = this.hasOwnProperty(prop);
         if (ownProp) {
-            const label = `${prop}`.charAt(0).toUpperCase() + `${prop}`.slice(1);
-            info.textContent = `${label}: ${this[prop]}`; 
+          const info = document.createElement('p');
+          info.classList.add('itemInfo');
+          div0.appendChild(info);  
+          const label = `${prop}`.charAt(0).toUpperCase() + `${prop}`.slice(1);
+          info.textContent = `${label}: ${this[prop]}`; 
         }
     }
     const read = document.createElement('label');
+    const div1 = document.createElement('div');
+    card.appendChild(div1);
+    div1.classList.add('rstatus');
     read.setAttribute('for', 'read');
     read.textContent = 'Read: ';
-    card.appendChild(read);
+    div1.appendChild(read);
     const status = document.createElement('input');
     status.setAttribute('type', 'checkbox');
     status.setAttribute('id', 'read');
     status.setAttribute('name', 'read_or_not');
-    card.appendChild(status);
+    div1.appendChild(status);
 
     const deleteBtn = document.createElement('button');
+    const div2 = document.createElement('div');
+    div2.classList.add('dltbtn');
+    card.appendChild(div2);
     deleteBtn.textContent = 'delete';
-    card.appendChild(deleteBtn);
+    div2.appendChild(deleteBtn);
     deleteBtn.addEventListener('click', () => {
       cards.removeChild(card);
       const indexD = myLibrary.findIndex(this);
@@ -62,8 +71,8 @@ function showLibrary() {
 
 showLibrary();
 
-//use form info to create a book object and display it on the page
-function storeNGetFormInfo(event){
+//use submitted form info to create another book object and display it on the page
+function GetFormInfo(event){
   const authorNew = document.querySelector('#author').value;
   const titleNew = document.querySelector('#title').value;
   const yearNew = document.querySelector('#year').value;
@@ -74,8 +83,9 @@ function storeNGetFormInfo(event){
 }
 
 const addBook = document.querySelector('form');
-addBook.addEventListener('submit', storeNGetFormInfo);
+addBook.addEventListener('submit', GetFormInfo);
 
+//update the newly added book's read status
 function readChecked () {
   const readStatus = document.getElementById('readOrNot');
   const checkedBox = document.querySelector('.display div:last-child #read');
@@ -85,5 +95,5 @@ function readChecked () {
 }
 addBook.addEventListener('submit', readChecked);
 
-const animalFarmStatus = document.querySelector('.display div:nth-child(2) #read');
+const animalFarmStatus = document.querySelector('.display div.libBook:nth-child(2) #read');
 animalFarmStatus.checked = true;
